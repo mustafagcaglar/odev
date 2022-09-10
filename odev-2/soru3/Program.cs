38d47e1d243db4d02a57490d0bb9bd99a2168f85
Mustafa Gokturk Caglar <mustafagcaglar@gmail.com>
2022-09-10 19:03:35 +0300

    odev-2 projeler eklendi

---
+++ odev-2/soru3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+Console.Write("Cümle Giriniz: ");
+string cumle = Console.ReadLine().ToLower();
+char[] sesliler = { 'a', 'e', 'ı', 'i', 'o', 'ö', 'u', 'ü' };
+ArrayList sesliHarf = new();
+foreach(char karakter in cumle)
+{
+    foreach(char sesli in sesliler)
+    {
+        if(karakter == sesli)
+        {
+            sesliHarf.Add(karakter);
+        }
+    } 
+}
+sesliHarf.Sort();
+foreach (char eleman in sesliHarf)
+{
+    System.Console.Write(eleman + " ");
+}
+
